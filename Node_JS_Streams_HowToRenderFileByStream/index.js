a449cda0fbaf0aaeaaca81bf87f067ac8fa8cb6f
MohammadShujaullah <mdshujaullah789@gmail.com>
2026-02-09 13:16:04 +0530

    we are using the (Streams) to render the file at the browser,as it sends the data in the form of (chunks), and also we used (express-status-monitor), which is used to know the CPU,Strorage usage by Express file, and we also convert the file (through read the file through streams->then it converts the file into zip)  without using the storage

---
+++ Node_JS_Streams_HowToRenderFileByStream/index.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import fs from 'fs';
+
+import zlib from "zlib";
+
+import expressmonitor from 'express-status-monitor'
+const PORT = 3000
+const app = express()
+
+
+app.use(expressmonitor());
+
+fs.createReadStream("./index.html").pipe(zlib.createGzip()).pipe(fs.createWriteStream("./index.zip"));
+
+
+app.get('/', (req, res) => {
+    const stream = fs.createReadStream("./index.html", "utf-8");
+
+    stream.on("data", (chunk) => res.write(chunk));
+
+    stream.on("end", () => res.end());
+
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
